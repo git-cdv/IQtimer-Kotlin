6625ab621480f5bb5c86fc82397c6d7ba2d849f1
chkan <chkan.dv@gmail.com>
2023-01-16 12:57:55 +0200

    ++ Updated UI create new Goal

--- app/src/main/java/com/chkan/iqtimer/ui/progress/GoalBottomFragment.kt
+++ app/src/main/java/com/chkan/iqtimer/ui/progress/GoalBottomFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import com.chkan.iqtimer.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.textfield.TextInputLayout
@@ -14,7 +12,8 @@ import android.graphics.Color
 import android.graphics.PorterDuff
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
+import android.widget.*
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.activityViewModels
 import com.chkan.iqtimer.domain.models.GoalModel
 import com.chkan.iqtimer.ui.progress.vm.ProgressViewModel
@@ -41,19 +40,8 @@ class GoalBottomFragment : BottomSheetDialogFragment() {
         bottomSheet.backgroundTintMode = PorterDuff.Mode.CLEAR
         bottomSheet.backgroundTintList = ColorStateList.valueOf(Color.TRANSPARENT)
         bottomSheet.setBackgroundColor(Color.TRANSPARENT)
-
-        val items = listOf(resources.getString(R.string.text_Session), resources.getString(R.string.text_effectiv_days))
-        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
-        val type = view.findViewById<TextInputLayout>(R.id.goal_field_type).editText as? AutoCompleteTextView
-        type?.setAdapter(adapter)
-        type?.setOnItemClickListener { _, _, position, _ ->
-            selectedType = if (position == 0) {
-                0
-            } else {
-                1
-            }
-        }
-
+        val radioGroup = view.findViewById<RadioGroup>(R.id.goal_radioGroup)
+        val radioSes = view.findViewById<RadioButton>(R.id.radioSessions)
         val et_plan = view.findViewById<TextInputEditText>(R.id.goal_et_quantity)
         val et_days = view.findViewById<TextInputEditText>(R.id.goal_et_days)
         val et_name = view.findViewById<TextInputEditText>(R.id.goal_name)
@@ -67,7 +55,8 @@ class GoalBottomFragment : BottomSheetDialogFragment() {
             if(validateInput(et_plan,til_plan, et_days, til_days)){
                 val name = et_name.text.toString()
                 val plan = et_plan.text.toString()
-                val _type = if(selectedType == 0) resources.getString(R.string.text_Session) else resources.getString(R.string.text_effectiv_days)
+                selectedType = if(radioGroup.checkedRadioButtonId==radioSes.id) 0 else 1
+                val _type = if(selectedType == 0) resources.getString(R.string.sessiy) else resources.getString(R.string.powerdays)
                 val days = et_days.text.toString()
                 val desc =
                     "${resources.getString(R.string.text_my_goal)} $plan $_type ${resources.getString(
@@ -85,33 +74,17 @@ class GoalBottomFragment : BottomSheetDialogFragment() {
         return when {
             quantity.text.isNullOrEmpty() -> {
                 til_quantity.error = resources.getString(R.string.empty)
-                setTextListener(quantity, til_quantity)
+                quantity.doAfterTextChanged { til_quantity.error = null }
                 false
             }
             days.text.isNullOrEmpty() -> {
                 til_days.error = resources.getString(R.string.empty)
-                setTextListener(days, til_days)
+                days.doAfterTextChanged { til_days.error = null }
                 false
             }
             else -> {
                 true
             }
         }
-    }
-
-    private fun setTextListener(editText: TextInputEditText, inputLayout: TextInputLayout) {
-        val mTextWatcher: TextWatcher = object : TextWatcher {
-            override fun afterTextChanged(s: Editable) {}
-            override fun beforeTextChanged(
-                s: CharSequence, start: Int,
-                count: Int, after: Int
-            ) {
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                inputLayout.error = null
-            }
-        }
-        editText.addTextChangedListener(mTextWatcher)
     }
 }
