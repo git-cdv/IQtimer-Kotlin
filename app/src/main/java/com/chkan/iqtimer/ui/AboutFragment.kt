3396f22a6ffcc3d1d1130b2fca0cba88afd2613c
chkan <chkan.dv@gmail.com>
2023-01-20 14:18:19 +0200

    Created About Screen

--- app/src/main/java/com/chkan/iqtimer/ui/AboutFragment.kt
+++ app/src/main/java/com/chkan/iqtimer/ui/AboutFragment.kt
@@ -1,10 +1,16 @@
 package com.chkan.iqtimer.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import com.chkan.iqtimer.R
 
 class AboutFragment : Fragment() {
@@ -14,6 +20,28 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val feedbackImage = view.findViewById<ImageView>(R.id.iv_feedback)
+        val feedbackText = view.findViewById<TextView>(R.id.tv_feedback)
+        val sendMail = {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject_mail))
+                data = Uri.parse("mailto: chkan.dv@gmail.com")
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)}
+            startActivity(intent)
+        }
+        feedbackImage.setOnClickListener {
+            sendMail.invoke()
+        }
+        feedbackText.setOnClickListener {
+            sendMail.invoke()
+        }
+
     }
 
 }
